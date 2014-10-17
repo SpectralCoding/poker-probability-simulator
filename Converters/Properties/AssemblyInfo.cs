7b4459ed594be1e28c92618313ba4f576ff9ed0b
Caesar Kabalan <caesar.kabalan@gmail.com>
2014-10-16 20:27:23 -0700

    Initial commit of blank project with initial setup done.

---
+++ Converters/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Poker Probability Simulator Converters Library")]
+[assembly: AssemblyDescription("Converters Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SpectralCoding")]
+[assembly: AssemblyProduct("Poker Probability Simulator")]
+[assembly: AssemblyCopyright("Copyright© 2014")]
+[assembly: AssemblyTrademark("All Rights Reserved")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2c3bf015-b291-454b-8aa4-8a9d82b10a87")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.1.16359")]
+[assembly: AssemblyFileVersion("0.0.1.16359")]
